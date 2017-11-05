962797dd9f5494ece751f2e1a340b715e5ea5174
Rafael D Ortiz <rafaeldemitri4@gmail.com>
2017-11-05 09:21:44 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,87 @@
+var friends = [
+
+    {
+        name: "Carlton",
+        photo: "https://vignette.wikia.nocookie.net/uncyclopedia/images/6/6e/CarltonBanks.jpg/revision/latest/scale-to-width-down/180?cb=20060508113424",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Will",
+        photo: "https://www.thesun.co.uk/wp-content/uploads/2017/08/nintchdbpict000289398111.jpg?strip=all&w=960",
+        scores: [
+            "5",
+            "3",
+            "1",
+            "2",
+            "4",
+            "4",
+            "1",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Brad",
+        photo: "http://images5.fanpop.com/image/photos/30800000/Everybody-Loves-Raymond-brad-garrett-30859457-480-600.jpg",
+        scores: [
+            "1",
+            "4",
+            "3",
+            "5",
+            "5",
+            "2",
+            "3",
+            "4",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Prince",
+        photo: "http://cdn.cnn.com/cnnnext/dam/assets/160421140425-restricted-42-prince-file-super-169.jpg",
+        scores: [
+            "2",
+            "2",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Janet",
+        photo: "http://media.tmz.com/2014/12/13/janet-jackson-200x250.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+
+
+];
+
+module.exports = friends;
